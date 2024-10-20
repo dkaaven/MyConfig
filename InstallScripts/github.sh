be9c48b251de0b2989a5fd1e7f5e4ba8f6a8d6c6
Daniel Kåven <daniel@kaaven.se>
2024-10-20 17:05:25 +0200

    Update github.sh

--- InstallScripts/github.sh
+++ InstallScripts/github.sh
@@ -1,2 +1,24 @@
 #TODO: Check linux-distro
-sudo pacman -S 
+sudo pacman -S git --noconfirm
+
+# fullname="USER INPUT"
+read -p "Enter fullname: " $fullname
+# user="USER INPUT"
+read -p "Enter email: " $email
+read -p "Standard folder: $gitrep
+
+echo "You have typed"
+echo "Name:  " $fullname
+each "Email: " $email
+read -p "Confirm (Y/N): " confirm && [[ $confirm == [yY] ]] || exit 1
+
+mkdir ~/$gitrep
+cd ~/$gitrep
+git config --global user.name fullname
+git config --global user.email email
+
+echo "You have installed Git and your work folder is ~/" $gitrep
+
+#Register ssh
+ssh-keygen
+ssh-copy-id username@remote_host
