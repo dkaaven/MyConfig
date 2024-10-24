6ce6da01ffc132de84fa9175194c4cd57137defd
Daniel Kåven <daniel@kaaven.se>
2024-10-24 18:54:21 +0200

    Update docker.sh

--- InstallScripts/docker.sh
+++ InstallScripts/docker.sh
@@ -5,7 +5,11 @@ sudo pacman -S docker docker-compose --noconfirm
 sudo systemctl start docker.service
 sudo systemctl enable docker.service
 
+#add user to docker group
 sudo usermod -aG docker $USER
 newgrp docker
+
+sudo pacman -S nvidia-container-toolkit
+
 # Test docker
 # docker run hello-world
